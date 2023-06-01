36a819c40776fdb6ed089af500402a008fa4a460
Masum267 <abdullahalmasum267@gmail.com>
2023-06-01 15:14:13 +0600

    Change About p

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
     return (
-        <div 
+        <div
             name="about"
             className='w-full h-screen bg-gradient-to-b from-gray-800
                 to-black text-slate-100'>
@@ -16,26 +16,23 @@ function About() {
                 </div>
 
                 <p className='text-xl mt-20'>
-                    Lorem, ipsum dolor sit amet consectetur 
-                    adipisicing elit. Ducimus cupiditate eveniet 
-                    provident dolores vitae ad amet repudiandae 
-                    illum consequuntur magni? Repellendus unde quod 
-                    autem doloremque nihil dignissimos eos vel pariatur 
-                    ut, culpa eligendi delectus. Voluptas minima quia 
-                    minus, laudantium fugit cupiditate optio ullam error 
-                    iure iusto assumenda consequuntur animi voluptates?
+                    Passionate Frontend Engineer who Proves skill 
+                    rather than showing skills. A developer who tries 
+                    to provide a simple and bugless code. A learner 
+                    with fast learning skills and lots of patience in 
+                    learning new things.
                 </p>
 
                 <br />
 
                 <p className='text-xl'>
-                    Lorem, ipsum dolor sit amet consectetur 
-                    adipisicing elit. Ducimus cupiditate eveniet 
-                    provident dolores vitae ad amet repudiandae 
-                    illum consequuntur magni? Repellendus unde quod 
-                    autem doloremque nihil dignissimos eos vel pariatur 
-                    ut, culpa eligendi delectus. Voluptas minima quia 
-                    minus, laudantium fugit cupiditate optio ullam error 
+                    Lorem, ipsum dolor sit amet consectetur
+                    adipisicing elit. Ducimus cupiditate eveniet
+                    provident dolores vitae ad amet repudiandae
+                    illum consequuntur magni? Repellendus unde quod
+                    autem doloremque nihil dignissimos eos vel pariatur
+                    ut, culpa eligendi delectus. Voluptas minima quia
+                    minus, laudantium fugit cupiditate optio ullam error
                     iure iusto assumenda consequuntur animi voluptates?
                 </p>
 
